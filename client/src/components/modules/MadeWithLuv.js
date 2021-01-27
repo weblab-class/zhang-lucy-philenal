543e2aa97da7f5d3dcda92fd45933d9dd1aa728a
philenal <philena.liu@gmail.com>
2021-01-26 22:59:06 -0600

    made with love

---
+++ client/src/components/modules/MadeWithLuv.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "../../../utilities.css";
+import "./PlayerPanel.css";
+
+class MadeWithLuv extends Component {
+  constructor(props) {
+    super(props);
+    // Initialize Default State
+    this.state = {
+    };
+  }
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <>
+        <div className="MadeWithLuv">
+          made with 
+        </div>
+      </>
+    );
+  }
+}
+
+export default MadeWithLuv;
